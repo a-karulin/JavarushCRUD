477f4cfd827c3193001991aba1feaa542c00304e
a-karulin <a_karulin@mail.ru>
2017-07-02 13:53:18 +0300

    Add Sql script

---
+++ sqlScripts/fillUserTable.sql
@@ -0,0 +1,5 @@
+insert into test.user values (null,'Willis',37,true,null),(null,'Zack',20,false,null),
+  (null,'Mike',28,false,null),(null,'Ostin',21,false,null),(null,'Nick',40,false,null),
+  (null,'John',43,true,null),(null,'Oscar',18,false,null),(null,'Scott',29,false,null),
+  (null,'Jany',23,false,null),(null,'Alice',32,false,null),(null,'Miranda',40,true,null),
+  (null,'Jackson',49,true,null),(null,'Emma',18,false,null),(null,'Tony',43,false,null);
